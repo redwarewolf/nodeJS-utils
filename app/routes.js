347dbf23806dc423d8a8db73b6d2ce4263e5d5c1
redwarewolf <jarapedro96@gmail.com>
2020-06-23 12:51:22 -0300

    Added readme

--- app/routes.js
+++ app/routes.js
@@ -1,9 +1,7 @@
 const usersController = require('./controllers/users');
-const { healthCheck } = require('./controllers/healthCheck');
 
 const URL = '/api/v1';
 
 exports.init = app => {
-  app.get('/health', healthCheck);
   app.get(`${URL}/users`, usersController.getUsers);
 };
